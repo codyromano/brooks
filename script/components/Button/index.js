b895b4363d6a57bc94a97155997e4df6344d62a4
Cody Romano <codyromano@gmail.com>
2017-12-21 04:31:08 -0800

    Improve button style

--- script/components/Button/index.js
+++ script/components/Button/index.js
@@ -1,12 +1,20 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import './Button.scss';
 
+const getClassName = ({ size = 'normal' }) =>
+  `nyt-button nyt-button-${size}`;
+
 const Button = (props) => (
   <button
-    className="nyt-button"
+    className={getClassName(props)}
     {...props}
   >{props.children}</button>
 );
+
+Button.propTypes = {
+  size: PropTypes.oneOf(['small', 'normal', 'large'])
+};
 
 export default Button;
