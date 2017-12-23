a36bced9d1120c3e1ee7f987ff8cdcb23f91a50a
Cody Romano <codyromano@gmail.com>
2017-12-22 22:53:28 -0500

    Add progress message

--- script/components/TableOfContents/index.js
+++ script/components/TableOfContents/index.js
@@ -4,11 +4,20 @@ import BrooksDataProvider from '../BrooksDataProvider';
 import Button from '../Button';
 import Header from '../Header';
 import Page, { PageWidthContainer } from '../Page';
+import withLibraryModel from '../../data/models/withLibraryModel';
 import './TableOfContents.scss';
 
-const TableOfContentsView = ({ contents }) => (
-  <Page>
+const TableOfContentsView = ({ articlesRead, contents }) => {
+  const completion = Math.round((articlesRead / contents.length) * 100);
+
+  return (<Page>
     <Header title="Letters" />
+
+    <PageWidthContainer>
+      <p>You've read {articlesRead} out of {contents.length} letters.
+       Read them all to unlock a reward.</p>
+    </PageWidthContainer>
+
     <PageWidthContainer>
       <ul className="nyt-table">
         {contents.map((article, i) => (
@@ -20,14 +29,15 @@ const TableOfContentsView = ({ contents }) => (
         ))}
       </ul>
     </PageWidthContainer>
-  </Page>
-);
+  </Page>);
+};
 
-const TableOfContents = () => (
+const TableOfContents = ({ articleLibraryModel }) => (
   <BrooksDataProvider
     endpoint="http://localhost:9980/table-of-contents"
     onDataReadyComponent={TableOfContentsView}
+    articlesRead={articleLibraryModel.getTotalArticlesRead()}
   />
 );
 
-export default TableOfContents;
+export default withLibraryModel(TableOfContents);
