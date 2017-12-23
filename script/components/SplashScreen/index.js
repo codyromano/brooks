4c8abf3e6e0444c6e69c4a3ce38d5e1e42f6f102
Cody Romano <codyromano@gmail.com>
2017-12-23 10:40:25 -0500

    Show different screen when no more articles visible

--- script/components/SplashScreen/index.js
+++ script/components/SplashScreen/index.js
@@ -2,12 +2,12 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import SplashScreenView from './SplashScreenView';
 import withCountdown from '../withCountdown';
+import BrooksDataProvider from '../BrooksDataProvider';
 
 const CountdownView = ({ secondsLeft }) => (
   <SplashScreenView
     title="David is writing..."
     about={`A new column will arrive in ${secondsLeft} seconds.`}
-    backgroundImageSrc="./images/paperplane.gif"
   />
 );
 
@@ -15,11 +15,31 @@ const Countdown = withCountdown(CountdownView);
 
 class SplashScreen extends React.Component {
   render() {
+    // TODO: Derive articles visible from localStorage
+    const articlesVisible = 0;
+    const totalArticles = this.props.contents.length;
+
+    if (articlesVisible < totalArticles) {
+      // TODO: Derive target time from localStorage
+      return (
+        <Countdown targetTime={1514051871809} />
+      );
+    }
     return (
-      <Countdown targetTime={1514051871809} />
+      <SplashScreenView
+        title="Nothing left to write."
+        about={`You've unlocked all of David's columns!`}
+      />
     );
   }
 }
 
+const SplashScreenWithData = () => (
+  <BrooksDataProvider
+    endpoint="http://localhost:9980/table-of-contents"
+    onDataReadyComponent={SplashScreen}
+  />
+);
+
 // 1514041871809
-export default SplashScreen;
+export default SplashScreenWithData;
